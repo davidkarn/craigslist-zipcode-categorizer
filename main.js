b302b00043e7ef07c2faadb54eb453bcea14293e
David Karn <davidkarnseattle@gmail.com>
2014-01-07 18:01:17 -0800

    wrote out zip-tallying code

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ var cl = require('craigslist-utils');
 var g2z = require('Gps2zip');
 
 var cities = cl.all_cities("US");
+var zips = {};
 
-function get_coordinates(next, city, sub_city) { 
+function get_zips(city, sub_city, next) { 
     function n(posts) {
 	next(posts.map(function(p) {
 	    if (!p.coords) { return false; }
@@ -13,5 +14,38 @@ function get_coordinates(next, city, sub_city) {
 	    .filter(function(x) { return x; })); }
     cl.get_posts(n, city, "apa", sub_city); }
 
-get_coordinates(console.log, "seattle");
+function associate_zips(city, sub_city, next) {
+    get_zips(city, sub_city,
+	     function(zips) {
+		 zips.map(function(z) {
+		     zips[z] = zips[z] || [];
+		     zips[z].push([city, sub_city]); }); 
+	     next(); }); }
+
+function get_all_location_combos() {
+    var combos = [];
+    for (var c in cities) {
+	var city = cities[c];
+	if (city.areas.length == 0) {
+	    combos.push([city.subdomain, false]); }
+	else {
+	    for (var a in city.areas) {
+		combos.push([city.subdomain, city.areas[a].path]); }}}
+    return combos; }
+
+var fns = get_all_location_combos().map(function(x) {
+    return {fn: associate_zips, args: x};});
+
+fns.push({fn: function() {console.log(zips); }, args: []});
+
+function call_next_in_chain() {
+    if (fns.length == 0) {
+	return; }
+
+    var fn = fns.shift();
+    fn.args.push(call_next_in_chain);
+    fn.fn.apply(false, fn.args); }
+
+call_next_in_chain();
+	
 	
