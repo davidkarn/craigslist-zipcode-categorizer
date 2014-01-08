d696757ceabe74173063059863c6b93b6ddc1968
David Karn <davidkarnseattle@gmail.com>
2014-01-07 17:26:46 -0800

    wrote get_coordinates()

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 var cl = require('craigslist-utils');
+var g2z = require('Gps2zip');
 
+var cities = cl.all_cities("US");
+
+function get_coordinates(next, city, sub_city) { 
+    function n(posts) {
+	next(posts.map(function(p) {
+	    if (!p.coords) { return false; }
+	    var r = g2z.gps2zip(p.coords.latitude, p.coords.longitude);
+	    if (r.error) { return false; }
+	    return r['zip code']; })
+	    .filter(function(x) { return x; })); }
+    cl.get_posts(n, city, "apa", sub_city); }
+
+get_coordinates(console.log, "seattle");
+	
